94be5307f57048d47d959b3cc6486d3b61f7da6e
Davitmach <wvime30@gmail.com>
2025-05-12 19:11:22 +0400

    fix: add max button to withdraw input for convenience

--- app/components/shared/withdraw.tsx
+++ app/components/shared/withdraw.tsx
@@ -7,6 +7,7 @@ import { useBalanceStore, useLanguageStore, UserData, useTransactionStore } from
 export const Withdraw = () => {
     const setWithdrawals = useTransactionStore((s) => s.setWithdrawals);
     const {id,initData} = UserData()
+    const {balance} = useBalanceStore();
  const fetchWithdrawals = async () => {
       try {
         const res = await fetch(`https://api.durowin.xyz/withdraws/get_user_list/${id}/${encodeURIComponent(initData)}`);
@@ -95,6 +96,9 @@ const {language} = useLanguageStore();
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
           />
+          <button onClick={()=>{
+            setAmount(balance.toString())
+          }} className='absolute right-[15px] cursor-pointer top-[50%] translate-y-[-50%]'>max</button>
           <svg className="absolute top-[50%] translate-y-[-50%] left-[19px]" width="18" height="18" viewBox="0 0 18 18" fill="none">
             <path d="M17.661 4.22L9.81501 16.714C9.71904 16.8653 9.58627 16.9898 9.42911 17.0759C9.27195 17.162 9.09554 17.2068 8.91636 17.2062C8.73717 17.2056 8.56107 17.1596 8.40449 17.0725C8.24792 16.9853 8.11598 16.8599 8.02102 16.708L0.328016 4.214C0.112094 3.86425 -0.00153249 3.46102 1.56103e-05 3.05C0.00922894 2.4428 0.259239 1.86413 0.695059 1.44125C1.13088 1.01837 1.71682 0.785909 2.32402 0.794995H15.686C16.963 0.793995 18 1.8 18 3.044C18 3.457 17.884 3.865 17.661 4.22ZM2.21802 3.8L7.94102 12.626V2.912H2.81602C2.22402 2.912 1.95902 3.304 2.21802 3.802M10.058 12.628L15.783 3.8C16.048 3.303 15.777 2.91 15.184 2.91H10.06L10.058 12.628Z" fill="white" />
           </svg>
